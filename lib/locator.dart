8aaff546aadd1d9bb5aded9e21393584fa0bff58
jiayenbeh <jiayenbeh@gmail.com>
2024-07-23 15:10:04 +0800

    Added a locator for different services

---
+++ lib/locator.dart
@@ -0,0 +1,14 @@
+import 'package:get_it/get_it.dart';
+import 'package:ai_teacher_chatbot/services/authentication_service.dart';
+import 'package:ai_teacher_chatbot/services/firestore_service.dart';
+import 'package:ai_teacher_chatbot/services/navigation_service.dart';
+import 'package:ai_teacher_chatbot/services/cloud_storage_service.dart';
+
+GetIt locator = GetIt.instance;
+
+void setupLocator() {
+  locator.registerLazySingleton(() => NavigationService());
+  locator.registerLazySingleton(() => AuthenticationService());
+  locator.registerLazySingleton(() => FirestoreService());
+  locator.registerLazySingleton(() => CloudStorageService());
+}
